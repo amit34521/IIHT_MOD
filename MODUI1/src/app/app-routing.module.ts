3728fca5e222ca7623b23628be1aed0cbf38e62d
Amit Panigrahi <amit.panigrahi@cognizant.com>
2019-12-16 18:13:37 +0530

    add

--- MODUI1/src/app/app-routing.module.ts
+++ MODUI1/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UserSignupComponent} from './signup/user/user-signup/user-signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  {
+    path:'usersignup',
+    component : UserSignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
